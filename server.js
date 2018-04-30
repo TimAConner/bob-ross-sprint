6007681cf28b9ba9b9c383f5a36a6678d1143e0a
Tim A. Conner <timaconner1@gmail.com>
2018-04-30 11:36:52 -0500

    Add server rendering static page and connect sass.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/client'));
+
+
+app.listen(8080, () => {
+  console.log("App is listening to port 8080");
+});
