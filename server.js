a74114b9bfe9c2a771ebbadbc13b7b526cb488a3
Tim A. Conner <timaconner1@gmail.com>
2018-04-30 13:08:27 -0500

    Add /movies route.

--- server.js
+++ server.js
@@ -2,9 +2,19 @@
 
 const express = require('express');
 const app = express();
+const movieApi = require('imdb-api');
 
 app.use(express.static(__dirname + '/client'));
 
+app.get('/movies', (req, res, next) => {
+  movieApi.search(
+    { title: req.query.keywords },
+    require('./server/key.js')
+  ).then(data => {
+    console.log(data);
+    res.status(200).json(data);
+  });
+});
 
 app.listen(8080, () => {
   console.log("App is listening to port 8080");
